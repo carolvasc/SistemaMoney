3872d3544f9bda195677d570c7bb85e5c1cc063d
Carolina Vasconcelos <cvsouza04@gmail.com>
2018-02-11 19:09:19 -0200

    Edição do método doGet para fazer a inserção na tabela

--- SistemaMoney/src/br/uninove/financeiro/controller/DespesaController.java
+++ SistemaMoney/src/br/uninove/financeiro/controller/DespesaController.java
@@ -1,12 +1,18 @@
 package br.uninove.financeiro.controller;
 
+import br.uninove.financeiro.dao.DespesaDAO;
+import br.uninove.financeiro.objetos.entidade.Despesa;
+
 import java.io.IOException;
+import java.sql.Date;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 //localhost:8080/SistemaMoney/despcontroller
 
@@ -18,9 +24,35 @@ public class DespesaController extends HttpServlet{
 	}
 	
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
 		
-		System.out.println("Chamou");
+		String nomeDespesa = req.getParameter("descricao");
+		Float valorDespesa = Float.parseFloat(req.getParameter("valor"));
+		Date dataDespesa = null; //req.getParameter("data");
+		Integer catDespesa = 1; //req.getParameter("");
+		Integer pagDespesa = 1; //req.getParameter("");
+		//String repDespesa = "Não repetir"; //req.getParameter("");
+		String obsDespesa = req.getParameter("observacao");
+		
+		Despesa desp = new Despesa();
+		
+		desp.setNomeDespesa(nomeDespesa);
+		desp.setValorDespesa(valorDespesa);
+		desp.setDataDespesa(dataDespesa);
+		desp.setCategoriaDespesa(catDespesa);
+		desp.setPagamentoDespesa(pagDespesa);
+		//desp.setRepetirDespesa(repDespesa);
+		desp.setObsDespesa(obsDespesa);
+		
+		try {
+			DespesaDAO despDAO = new DespesaDAO();
+			despDAO.salvar(desp);
+			resp.getWriter().print("Cadastrado com sucesso");
+			System.out.println("Despesa cadastrada com sucesso!");
+		} catch (SQLException e) {
+			resp.getWriter().print("Ocorreu algum erro");
+			e.printStackTrace();
+		}
 		
 	}
 	
