9205396b7242f00124a31b26ab100f994a1401f4
Carolina Vasconcelos <cvsouza04@gmail.com>
2018-02-12 14:31:41 -0200

    Cadastro da despesa via doPost e exclusão/listagem via doGet

--- SistemaMoney/src/br/uninove/financeiro/controller/DespesaController.java
+++ SistemaMoney/src/br/uninove/financeiro/controller/DespesaController.java
@@ -13,47 +13,69 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.util.List;
 
 //localhost:8080/SistemaMoney/despcontroller
 
 @WebServlet("/despcontroller")
-public class DespesaController extends HttpServlet{
+public class DespesaController extends HttpServlet {
 
-	public DespesaController(){
+	public DespesaController() {
 		System.out.println("Servlet");
 	}
-	
+
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		resp.setContentType("text/html");
 		
+		String acao = req.getParameter("acao");
+		DespesaDAO despDAO = new DespesaDAO();
+
+		// Se a ação for igual a excluir
+		if (acao.equals("excluir")) {
+			String id = req.getParameter("id");
+
+			Despesa desp = new Despesa();
+			if (id != null) {
+				desp.setIdDespesa(Integer.parseInt(id));
+			}
+
+			despDAO.excluir(Integer.parseInt(id));
+
+		} else if (acao.equals("listar")) {
+			// Se a ação for igual listar
+			List<Despesa> lista = despDAO.buscar();
+			for(Despesa d: lista)
+				resp.getWriter().print(d.getNomeDespesa() + "<br>");
+		}
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
 		String nomeDespesa = req.getParameter("descricao");
 		Float valorDespesa = Float.parseFloat(req.getParameter("valor"));
-		Date dataDespesa = null; //req.getParameter("data");
-		Integer catDespesa = 1; //req.getParameter("");
-		Integer pagDespesa = 1; //req.getParameter("");
-		//String repDespesa = "Não repetir"; //req.getParameter("");
+		Date dataDespesa = null;
+		Integer catDespesa = 1;
+		Integer pagDespesa = 1;
 		String obsDespesa = req.getParameter("observacao");
-		
+
 		Despesa desp = new Despesa();
-		
+
 		desp.setNomeDespesa(nomeDespesa);
 		desp.setValorDespesa(valorDespesa);
 		desp.setDataDespesa(dataDespesa);
 		desp.setCategoriaDespesa(catDespesa);
 		desp.setPagamentoDespesa(pagDespesa);
-		//desp.setRepetirDespesa(repDespesa);
 		desp.setObsDespesa(obsDespesa);
-		
-		try {
-			DespesaDAO despDAO = new DespesaDAO();
-			despDAO.salvar(desp);
-			resp.getWriter().print("Cadastrado com sucesso");
-			System.out.println("Despesa cadastrada com sucesso!");
-		} catch (SQLException e) {
-			resp.getWriter().print("Ocorreu algum erro");
-			e.printStackTrace();
-		}
-		
+
+		DespesaDAO despDAO = new DespesaDAO();
+		despDAO.salvar(desp);
+		resp.getWriter().print("Cadastrado com sucesso");
+		System.out.println("Despesa cadastrada com sucesso!");
+
 	}
-	
+
 }
