af2f046aba93a8658b54286addcb3f5b201c0150
Carol Vasconcelos <cvsouza04@gmail.com>
2018-03-30 13:53:24 -0300

    Revert "Refiz a tela de login e cadastro"

--- SistemaMoney/WebContent/resources/javascript/login.js
+++ SistemaMoney/WebContent/resources/javascript/login.js
@@ -1,27 +1,61 @@
-$(document).ready(function(){
-			$('#login').validate({
-				rules:{
-					lemail: {
-						required: true,
-						email: true
-					},
-					lsenha: {
-						required:true,
-						minlength: 7,
-						maxlength: 15
-					}
-				},
-				messages:{
-					lemail:{
-						required:'Por favor coloque um e-mail',
-						email: 'Digite um e-mail válido por favor'
-						},
-					lsenha:{
-						required:'Por favor coloque um e-mail',
-						minlength: 'Digite uma senha maior (min 7 caracteres)',
-						maxlength: 'Digite uma senha menor (max 15 caracteres)'
-						}
-					}	
-			});
+$(document).ready( function() {
+  $("#cadastro").validate({
+    // Define as regras
+    rules:{
+      nome:{
+        // campoNome será obrigatório (required) e terá tamanho mínimo (minLength)
+        required: true
+      },
+      email:{
+        // campoEmail será obrigatório (required) e precisará ser um e-mail válido (email)
+        required: true, email: true
+      },
+      senha:{
+        // campoMensagem será obrigatório (required) e terá tamanho mínimo (minLength)
+        required: true, minlength: 6
+      }
+    },
+    // Define as mensagens de erro para cada regra
+    messages:{
+      nome:{
+        required: "Digite o seu nome"
+      },
+      email:{
+        required: "Digite o seu e-mail",
+        email: "Digite um e-mail valido"
+      },
+      senha:{
+        required: "Digite uma senha",
+        minLength: "Sua senha deve ter 6 caracteres"
+      }
+    }
+  });
 });
-					
+
+$(document).ready( function() {
+	  $("#janela").validate({
+	    // Define as regras
+	    rules:{
+	      
+	      lemail:{
+	        // campoEmail será obrigatório (required) e precisará ser um e-mail válido (email)
+	        required: true
+	      },
+	      lsenha:{
+	        // campoMensagem será obrigatório (required) e terá tamanho mínimo (minLength)
+	        required: true
+	      }
+	    },
+	    // Define as mensagens de erro para cada regra
+	    messages:{
+	     
+	      lemail:{
+	        required: "Digite o seu e-mail"
+	      },
+	      lsenha:{
+	        required: "Digite uma senha"
+	
+	      }
+	    }
+	  });
+	});
